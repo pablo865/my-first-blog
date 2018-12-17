5b3f211a75c167bad8c6fd7df2071e042295754a
Pawel Piekutowski <futureiscode@gmail.com>
2018-12-17 15:24:09 +0100

    Komputer domowy

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lrspt$d%an8b3k+@%bcrnj_5f)c4_mdci&6ij2&8j-d$4e*$oo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["pablodjango86ad.pythonanywhere.com"]
+ALLOWED_HOSTS = ["pablo865.pythonanywhere.com"]
 
 
 # Application definition
